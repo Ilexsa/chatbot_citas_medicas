1eb350a2196c17a789ea8297e2809177fd477174
Ilexsax <jonathan_ayala2011@hotmail.com>
2025-08-07 20:45:54 -0500

    SE AGREGO EL PAQUETE PARA USAR LA API DE WHATSAPP

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/testei', function () {
+    return response()->json(['message' => 'Hello from testei']);
+});
